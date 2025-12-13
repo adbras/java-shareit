cc6b2a28bbad9efc77be367de899e7d41b70313d
abdras <aabdras@yandex.kz>
2025-12-13 22:43:15 +0500

    Финальное задание 16 спринта

---
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.request.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class ItemRequestDto {
+    private Long id;
+    private String description;
+    private UserDto requester;
+    private LocalDateTime created;
+    private List<ItemDto> items;
+}
